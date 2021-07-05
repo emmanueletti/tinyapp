01f2f527e3a45bdfda204c0eec1dee08654e53f9
Emmanuel Etti <emmanueletti@gmail.com>
2021-07-05 23:49:46 +0000

    created random string generator function

--- express_server.js
+++ express_server.js
@@ -1,21 +1,38 @@
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
 const app = express();
 const PORT = 8080;
+
+const generateRandomString = () => {
+  let output = '';
+
+  // generate 6 random value betweena ascii a(65) and z(122)
+  for (let i = 0; i < 6; i++) {
+    let randomNum = Math.floor(Math.random() * (122 - 97) + 97);
+    output += String.fromCharCode(randomNum);
+  }
+
+  console.log(output);
+};
+
+generateRandomString();
 
 // setting ejs as view engine
 app.set('view engine', 'ejs');
 
-// middleware
+// MIDDLE WARE
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// data
+// PSUEDO DATABASE
 const urlDatabase = {
   b2xVn2: 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com',
 };
 
-// routes
+// ROUTES
 app.get('/', (req, res) => {
   res.send('welcome to my server');
 });
@@ -29,8 +46,20 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new.ejs');
+});
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('got it');
+});
+
 app.get('/urls/:shortURL', (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+  };
 
   res.render('urls_show.ejs', templateVars);
 });
