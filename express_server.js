b824cdbd669edaa6bc01952e20065f38403007a3
Emmanuel Etti <emmanueletti@gmail.com>
2021-07-05 21:49:28 +0000

    created url route and ejs template display

--- express_server.js
+++ express_server.js
@@ -20,8 +20,9 @@ app.get('/', (req, res) => {
   res.send('welcome to my server');
 });
 
-app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World</b></body></html>\n');
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
 });
 
 app.get('/urls.json', (req, res) => {
