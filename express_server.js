ef837543ae991163a0777d857422fa1f7ad91803
Emmanuel Etti <emmanueletti@gmail.com>
2021-07-05 19:45:50 +0000

    created initial routes

---
+++ express_server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const PORT = 8080;
+
+// middleware
+// app.use(morgan('dev'));
+
+// data
+const urlDatabase = {
+  b2xVn2: 'http://www.lighthouselabs.ca',
+  '9sm5xK': 'http://www.google.com',
+};
+
+// routes
+app.get('/urlsss', (req, res) => {
+  res.send('welcome to my server');
+});
+
+app.use(function (req, res, next) {
+  res.status(404).send("Sorry can't find that!");
+});
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
